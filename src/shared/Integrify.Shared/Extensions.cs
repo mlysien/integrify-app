84c63fdde153d936474c685da1e6ded450c57d5d
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-03-06 20:01:36 +0100

    add missing NuGet packages

--- src/shared/Integrify.Shared/Extensions.cs
+++ src/shared/Integrify.Shared/Extensions.cs
@@ -1,5 +1,18 @@
-﻿namespace Integrify.Shared;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Integrify.Shared;
 
 public static class Extensions
 {
+    public static IServiceCollection AddSharedFramework(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+        
+    public static IApplicationBuilder UseSharedFramework(this IApplicationBuilder app)
+    {
+        return app;
+    }
 }
