a0067fd2582176307cc84b865d98c6255f14db74
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-03-08 17:47:26 +0100

    create abstraction for modules

---
+++ src/shared/Integrify.Shared.Abstractions/Modules/IModule.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Integrify.Shared.Abstractions.Modules;
+
+/// <summary>
+/// Marker interface for modules
+/// </summary>
+public interface IModule
+{
+    /// <summary>
+    /// Module name
+    /// </summary>
+    string Name { get; }
+    
+    /// <summary>
+    /// Registers module dependencies
+    /// </summary>
+    void Register(IServiceCollection services);
+    
+    /// <summary>
+    /// Adds module middleware 
+    /// </summary>
+    void Use(IApplicationBuilder app);
+}
