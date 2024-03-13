3e487b74e264ea4eb86f18c7fa79633d1c50b4eb
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-03-13 21:43:24 +0100

    create ModuleLoader

---
+++ src/shared/Integrify.Shared.Infrastructure/Modules/ModuleLoader.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using Integrify.Shared.Abstractions.Modules;
+using Microsoft.Extensions.Configuration;
+
+namespace Integrify.Shared.Infrastructure.Modules;
+
+public static class ModuleLoader
+{
+    public static IList<Assembly> LoadAssemblies(IConfiguration configuration, string modulePart)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+        var locations = assemblies.Where(x => !x.IsDynamic).Select(x => x.Location).ToArray();
+        var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+            .Where(x => !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase))
+            .ToList();
+
+        var disabledModules = new List<string>();
+        foreach (var file in files)
+        {
+            if (!file.Contains(modulePart))
+            {
+                continue;
+            }
+
+            var moduleName = file.Split(modulePart)[1].Split(".")[0].ToLowerInvariant();
+            var enabled = configuration.GetValue<bool>($"{moduleName}:module:enabled");
+            if (!enabled)
+            {
+                disabledModules.Add(file);
+            }
+        }
+
+        foreach (var disabledModule in disabledModules)
+        {
+            files.Remove(disabledModule);
+        }
+            
+        files.ForEach(x => assemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(x))));
+
+        return assemblies;
+    }
+}
