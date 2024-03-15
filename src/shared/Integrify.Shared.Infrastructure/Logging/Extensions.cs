9f086a7aec68a80f26d0bac392cd15d9091c1067
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-03-15 19:38:40 +0100

    add Serilog NuGet package for make logs more prettier

---
+++ src/shared/Integrify.Shared.Infrastructure/Logging/Extensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
+
+namespace Integrify.Shared.Infrastructure.Logging;
+
+public static class Extensions
+{
+    private const string ConsoleOutputTemplate = "{Timestamp:HH:mm:ss} [{Level:u3}] {Message}{NewLine}{Exception}";
+    private const string AppSectionName = "app";
+    private const string LoggerSectionName = "logger";
+    
+    public static IHostBuilder UseLogging(this IHostBuilder builder, Action<LoggerConfiguration>? configure = null,
+        string loggerSectionName = LoggerSectionName,
+        string appSectionName = AppSectionName)
+        => builder.UseSerilog((context, loggerConfiguration) =>
+        {
+            loggerConfiguration.WriteTo.Console(LogEventLevel.Information, ConsoleOutputTemplate);
+        });
+
+}
