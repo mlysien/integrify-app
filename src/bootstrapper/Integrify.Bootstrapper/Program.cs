6984adf06f8031dffb24a995057ede69ffa2495f
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-03-26 19:04:43 +0100

    update bootstrapper startup

--- src/bootstrapper/Integrify.Bootstrapper/Program.cs
+++ src/bootstrapper/Integrify.Bootstrapper/Program.cs
@@ -1,47 +1,16 @@
-using Integrify.Shared.Abstractions.Modules;
-using Integrify.Shared.Infrastructure;
 using Integrify.Shared.Infrastructure.Logging;
 using Integrify.Shared.Infrastructure.Modules;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Host
-    .ConfigureModules()
-    .UseLogging();
+namespace Integrify.Bootstrapper;
 
-var assemblies = ModuleLoader.LoadAssemblies(builder.Configuration, "Integrify.Modules.");
-var modules = ModuleLoader.LoadModules(assemblies);
-
-
-builder.Services.AddModularInfrastructure(assemblies, modules);
-
-foreach (var module in modules)
+public static class Program
 {
-    module.Register(builder.Services);
-}
+    public static Task Main(string[] args)
+        => CreateHostBuilder(args).Build().RunAsync();
 
-var app = builder.Build();
-PrintBanner();
-
-foreach (var module in modules)
-{
-    app.Logger.LogInformation($"Module {module.Name} [v.{module.Version}] has been loaded");
-}
-
-app.UseModularInfrastructure();
-app.Run();
-
-return;
-
-void PrintBanner()
-{
-    app.Logger.LogInformation("""
-                              
-                               _____ _   _ _______ ______ _____ _____  _____ ________     __
-                              |_   _| \ | |__   __|  ____/ ____|  __ \|_   _|  ____\ \   / /
-                                | | |  \| |  | |  | |__ | |  __| |__) | | | | |__   \ \_/ /
-                                | | | . ` |  | |  |  __|| | |_ |  _  /  | | |  __|   \   /
-                               _| |_| |\  |  | |  | |___| |__| | | \ \ _| |_| |       | |
-                              |_____|_| \_|  |_|  |______\_____|_|  \_\_____|_|       |_|
-                                                                                            
-                              """);
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+            .ConfigureModules()
+            .UseLogging();
 }
---
+++ src/bootstrapper/Integrify.Bootstrapper/Startup.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using Integrify.Shared.Abstractions.Modules;
+using Integrify.Shared.Infrastructure;
+using Integrify.Shared.Infrastructure.Contracts;
+using Integrify.Shared.Infrastructure.Modules;
+
+namespace Integrify.Bootstrapper;
+
+public class Startup
+{
+    private readonly IList<Assembly> _assemblies;
+    private readonly IList<IModule> _modules;
+
+    public Startup(IConfiguration configuration)
+    {
+        _assemblies = ModuleLoader.LoadAssemblies(configuration, "Integrify.Modules.");
+        _modules = ModuleLoader.LoadModules(_assemblies);
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddModularInfrastructure(_assemblies, _modules);
+        foreach (var module in _modules)
+        {
+            module.Register(services);
+        }
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+    {
+        logger.LogInformation($"Modules: {string.Join(", ", _modules.Select(x => $"{x.Name} ({x.Version})"))}");
+        app.UseModularInfrastructure();
+        foreach (var module in _modules)
+        {
+            module.Use(app);
+        }
+
+        app.ValidateContracts(_assemblies);
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapModuleInfo();
+        });
+
+        _assemblies.Clear();
+        _modules.Clear();
+    }
+}
