f15ca428f18debcb124bf45f7635106759bcb5ad
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-03 13:44:19 +0200

    add abstractions for order module

--- src/modules/order/Integrify.Modules.Order/OrderModule.cs
+++ src/modules/order/Integrify.Modules.Order/OrderModule.cs
@@ -1,5 +1,21 @@
-﻿namespace Integrify.Modules.Order;
+﻿using Integrify.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-public class OrderModule
+namespace Integrify.Modules.Order;
+
+public class OrderModule : IModule
 {
+    public string Name => "Order";
+    
+    public Version? Version => new(0, 0, 1);
+    
+    public void Use(IApplicationBuilder app)
+    {
+       
+    }
+
+    public void Register(IServiceCollection services)
+    {
+    }
 }
