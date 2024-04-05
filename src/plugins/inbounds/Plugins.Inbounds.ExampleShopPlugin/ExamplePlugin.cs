0d32b983beb26a789864364242cc9ed876062a01
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-05 17:43:58 +0200

    create example inbound plugin

---
+++ src/plugins/inbounds/Plugins.Inbounds.ExampleShopPlugin/ExamplePlugin.cs
@@ -0,0 +1,19 @@
+﻿using Integrify.Shared.Abstractions.Plugins;
+
+namespace Plugins.Inbounds.ExampleShopPlugin;
+
+public class ExamplePlugin : IPlugin
+{
+    public PluginType Type => PluginType.Inbound;
+    public string Name => "Example Inbound Plugin";
+    
+    public Task<TResult> FetchAsync<TResult>(CancellationToken cancellationToken = default) where TResult : class, IPluginContract
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendAsync<TObject>(TObject contractObject, CancellationToken cancellationToken = default) where TObject : class, IPluginContract
+    {
+        throw new NotImplementedException();
+    }
+}
