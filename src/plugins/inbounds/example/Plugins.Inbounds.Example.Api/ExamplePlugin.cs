599c4363b8e5bf8e8eeda1481d96e38108309fe9
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-08 21:38:37 +0200

    update ExamplePlugin implementation

--- src/plugins/inbounds/example/Plugins.Inbounds.Example.Api/ExamplePlugin.cs
+++ src/plugins/inbounds/example/Plugins.Inbounds.Example.Api/ExamplePlugin.cs
@@ -1,23 +1,19 @@
 ﻿using Integrify.Shared.Abstractions.Plugins;
+using Microsoft.Extensions.Logging;
 
 namespace Plugins.Inbounds.Example.Api;
 
 /// <summary>
 /// This is an example implementation of inbound type plugin.
 /// </summary>
-public class ExamplePlugin : IPlugin
+public class ExamplePlugin : IInboundPlugin
 {
-    public PluginType Type => PluginType.Inbound;
-
     public string Name => "Example Plugin";
-    
-    public Task<TResult> FetchAsync<TResult>(CancellationToken cancellationToken = default) where TResult : class, IPluginContract
-    {
-        throw new NotImplementedException();
-    }
 
-    public Task SendAsync<TObject>(TObject contractObject, CancellationToken cancellationToken = default) where TObject : class, IPluginContract
+    public PluginType Type => PluginType.Inbound;
+    
+    public Task FetchAsync()
     {
-        throw new NotImplementedException();
+        return Task.CompletedTask;
     }
 }
