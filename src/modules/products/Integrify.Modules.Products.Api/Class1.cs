b9e26eebac1fbeb14d43119eb2354361f14a4c18
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-15 19:20:32 +0200

    add Products module implementation

--- src/modules/products/Integrify.Modules.Products.Api/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Integrify.Modules.Products.Api;
-
-public class Class1
-{
-}
---
+++ src/modules/products/Integrify.Modules.Products.Api/ProductsModule.cs
@@ -0,0 +1,22 @@
+﻿using Integrify.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Integrify.Modules.Products.Api;
+
+public class ProductsModule : IModule
+{
+    public string Name => "Products";
+    
+    public Version? Version => new(0, 0, 1);
+    
+    public void Register(IServiceCollection services)
+    {
+        
+    }
+
+    public void Use(IApplicationBuilder app)
+    {
+
+    }
+}
