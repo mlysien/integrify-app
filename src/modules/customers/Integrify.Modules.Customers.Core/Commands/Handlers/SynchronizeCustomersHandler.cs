6c146f1963e1815b718b5c7ba9bfbfa0f5e9c7b0
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-21 13:51:50 +0200

    add logger for SynchronizeCustomersHandler

--- src/modules/customers/Integrify.Modules.Customers.Core/Commands/Handlers/SynchronizeCustomersHandler.cs
+++ src/modules/customers/Integrify.Modules.Customers.Core/Commands/Handlers/SynchronizeCustomersHandler.cs
@@ -1,13 +1,18 @@
 using Integrify.Modules.Customers.Core.Events;
 using Integrify.Shared.Abstractions.Commands;
 using Integrify.Shared.Abstractions.Messaging;
+using Microsoft.Extensions.Logging;
 
 namespace Integrify.Modules.Customers.Core.Commands.Handlers;
 
-internal class SynchronizeCustomersHandler(IMessageBroker messageBroker) : ICommandHandler<SynchronizeCustomers>
+internal class SynchronizeCustomersHandler(
+    ILogger<SynchronizeCustomersHandler> logger,
+    IMessageBroker messageBroker) : ICommandHandler<SynchronizeCustomers>
 {
     public async Task HandleAsync(SynchronizeCustomers command, CancellationToken cancellationToken = default)
     { 
+        logger.LogInformation("Customers synchronization started");
+        
         await messageBroker.PublishAsync(new CustomersSynchronizationRequested(), cancellationToken);
     }
 }
