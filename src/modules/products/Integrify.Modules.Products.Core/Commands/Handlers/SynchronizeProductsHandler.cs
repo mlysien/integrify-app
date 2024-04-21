9a556681fec572433352e0f55fc3ee8d5e4dbcc2
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-21 13:24:00 +0200

    add logger for SynchronizeProductsHandler

--- src/modules/products/Integrify.Modules.Products.Core/Commands/Handlers/SynchronizeProductsHandler.cs
+++ src/modules/products/Integrify.Modules.Products.Core/Commands/Handlers/SynchronizeProductsHandler.cs
@@ -1,11 +1,14 @@
 using Integrify.Shared.Abstractions.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace Integrify.Modules.Products.Core.Commands.Handlers;
 
-public class SynchronizeProductsHandler : ICommandHandler<SynchronizeProducts>
+public class SynchronizeProductsHandler(ILogger<SynchronizeProductsHandler> logger) : ICommandHandler<SynchronizeProducts>
 {
     public Task HandleAsync(SynchronizeProducts command, CancellationToken cancellationToken = default)
     {
+        logger.LogInformation("Products synchronization started");
+        
         throw new NotImplementedException();
     }
 }
