6e4651038fedb7216f2d5e3377e0e7664ad8148e
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-22 20:09:49 +0200

    add Extensions class with registered modules contracts

---
+++ src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Integrify.Modules.Synchronizer.Core.Contracts;
+using Integrify.Shared.Infrastructure.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Integrify.Modules.Synchronizer.Core;
+
+public static class Extensions
+{
+    public static void AddCoreLayer(this IServiceCollection services)
+    {
+    }
+
+    public static void UseCoreLayer(this IApplicationBuilder app)
+    {
+        app.UseContracts().Register<CustomersSynchronizationContract>();
+        app.UseContracts().Register<OrdersSynchronizationContract>();
+        app.UseContracts().Register<ProductsSynchronizationContract>();
+        app.UseContracts().Register<StocksSynchronizationContract>();
+    }
+}
+
