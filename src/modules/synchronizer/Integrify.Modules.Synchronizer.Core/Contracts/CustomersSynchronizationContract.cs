45215c35de259e1b1a686ea0b92485e1136f295d
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-22 20:08:11 +0200

    add Cusomers synchronization contract & event

---
+++ src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Contracts/CustomersSynchronizationContract.cs
@@ -0,0 +1,14 @@
+using Integrify.Modules.Synchronizer.Core.Events.External;
+using Integrify.Shared.Abstractions.Contracts;
+using Integrify.Shared.Abstractions.Messaging;
+
+namespace Integrify.Modules.Synchronizer.Core.Contracts;
+
+[Message("customers")]
+internal class CustomersSynchronizationContract: Contract<CustomersSynchronizationRequested>
+{
+    public CustomersSynchronizationContract()
+    {
+        RequireAll();
+    }
+}
