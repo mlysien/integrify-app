908326d8cd7689f389d8368857127c1f6e1efdd8
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-22 20:08:41 +0200

    add Stocks synchronization contract & event

---
+++ src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Contracts/StocksSynchronizationContract.cs
@@ -0,0 +1,14 @@
+using Integrify.Modules.Synchronizer.Core.Events.External;
+using Integrify.Shared.Abstractions.Contracts;
+using Integrify.Shared.Abstractions.Messaging;
+
+namespace Integrify.Modules.Synchronizer.Core.Contracts;
+
+[Message("stocks")]
+internal class StocksSynchronizationContract: Contract<StocksSynchronizationRequested>
+{
+    public StocksSynchronizationContract()
+    {
+        RequireAll();
+    }
+}
---
+++ src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Events/External/StocksSynchronizationRequested.cs
@@ -0,0 +1,5 @@
+using Integrify.Shared.Abstractions.Events;
+
+namespace Integrify.Modules.Synchronizer.Core.Events.External;
+
+public record StocksSynchronizationRequested : IEvent;
