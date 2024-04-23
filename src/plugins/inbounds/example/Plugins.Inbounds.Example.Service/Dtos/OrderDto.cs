303c51aabff7fc7b87fb7fe0cf94c3c9baeb252f
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-23 21:32:18 +0200

    rename Inbounds.Example plugin services project

--- src/plugins/inbounds/example/Plugins.Inbounds.Example.Service/Dtos/OrderDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Plugins.Inbounds.Example.Service.Dtos;
-
-public sealed class OrderDto
-{
-    public Guid OrderId { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-}
--- src/plugins/inbounds/example/Plugins.Inbounds.Example.Service/Services/Abstractions/IRestApiService.cs
+++
@@ -1,8 +0,0 @@
-using Plugins.Inbounds.Example.Service.Dtos;
-
-namespace Plugins.Inbounds.Example.Service.Services.Abstractions;
-
-public interface IRestApiService
-{
-    Task<IReadOnlyCollection<OrderDto>> GetOrders();
-}
