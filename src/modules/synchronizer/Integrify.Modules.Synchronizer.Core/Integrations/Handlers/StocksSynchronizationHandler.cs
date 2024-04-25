5031fa8c87bd4e37a5be2da6776ff4d25c14e101
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-04-25 21:29:00 +0200

    update namespaces for integration handlers

--- src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Integrations/Handlers/StocksSynchronizationHandler.cs
+++ src/modules/synchronizer/Integrify.Modules.Synchronizer.Core/Integrations/Handlers/StocksSynchronizationHandler.cs
@@ -1,7 +1,6 @@
-using Integrify.Modules.Synchronizer.Core.Integrations;
 using Integrify.Shared.Abstractions.Commands;
 
-namespace Integrify.Modules.Synchronizer.Core.Commands.Handlers;
+namespace Integrify.Modules.Synchronizer.Core.Integrations.Handlers;
 
 internal sealed class StocksSynchronizationHandler : ICommandHandler<StocksIntegration>
 {
