7291f6797c66518b1cef2d08900877fdca1c1b49
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-05-07 16:27:53 +0200

    update synchronization handler of customers

--- src/modules/customers/Integrify.Modules.Customers.Core/Commands/Handlers/SynchronizeCustomersHandler.cs
+++ src/modules/customers/Integrify.Modules.Customers.Core/Commands/Handlers/SynchronizeCustomersHandler.cs
@@ -1,28 +1,26 @@
 using Integrify.Modules.Customers.Core.Events;
 using Integrify.Modules.Customers.Port.Api;
 using Integrify.Shared.Abstractions.Commands;
-using Integrify.Shared.Abstractions.Messaging;
 using Microsoft.Extensions.Logging;
 
 namespace Integrify.Modules.Customers.Core.Commands.Handlers;
 
 internal class SynchronizeCustomersHandler(
     ILogger<SynchronizeCustomersHandler> logger,
-    ICustomerSourceApi sourceApi,
-    IMessageBroker messageBroker) : ICommandHandler<SynchronizeCustomers>
+    ICustomerSourceApi customerSourceApi,
+    ICustomerTargetApi customerTargetApi) : ICommandHandler<SynchronizeCustomers>
 {
     public async Task HandleAsync(SynchronizeCustomers command, CancellationToken cancellationToken = default)
     { 
         logger.LogInformation("Customers synchronization started");
-
-
-        var collection = await sourceApi.GetCustomersCollectionAsync();
-
-        foreach (var dto in collection)
-        {
-            var a = dto.Id;
-        }
         
-        await messageBroker.PublishAsync(new CustomersSynchronizationRequested(), cancellationToken);
+        var customersCollection = await customerSourceApi.GetCustomersCollectionAsync();
+
+        foreach (var customerDto in customersCollection)
+        {
+            logger.LogInformation("Processing Customer with Id: {0}", customerDto.Id);
+            
+            await customerTargetApi.SaveCustomerAsync(customerDto);
+        }
     }
 }
