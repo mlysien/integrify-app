5a21b71d3c8f612cea3c6e718206fa93f4276489
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-05-09 20:50:12 +0200

    create FakeErp plugin

---
+++ src/plugins/fakeerp/Plugins.FakeErp.Api/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Integrify.Shared.Abstractions.Plugins;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Plugins.FakeErp.Api;
+
+public class FakeErpPlugin : IPlugin
+{
+    public string Name => "FakeERP plugin";
+    public void Register(IServiceCollection services)
+    {
+    }
+
+    public void Use(IApplicationBuilder app)
+    {
+    }
+}
