030f6bb2f41b2f0079ec256843bf2769a9575e6d
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-05-15 22:32:45 +0200

    update solution file

--- src/integrations/customers/Integrify.Integrations.Customers.Core/Services/CustomersIntegration.cs
+++ src/integrations/customers/Integrify.Integrations.Customers.Core/Services/CustomersIntegration.cs
@@ -1,10 +1,8 @@
-using Integrify.Integrations.Customers.Port.Driven;
-using Integrify.Integrations.Customers.Port.Driving;
 using Microsoft.Extensions.Logging;
 
 namespace Integrify.Integrations.Customers.Core.Services;
 
-public class CustomersIntegration(ILogger<CustomersIntegration> logger) : ICustomersIntegration
+internal sealed class CustomersIntegration(ILogger<CustomersIntegration> logger) : ICustomersIntegration
 {
     public async Task RunIntegration()
     {
