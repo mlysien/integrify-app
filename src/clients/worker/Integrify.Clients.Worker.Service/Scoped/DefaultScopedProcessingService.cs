689f24e9137b3b250c42505c69a987d83bab1df4
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-05-17 15:21:57 +0200

    update worker client with new integrations commands

--- src/clients/worker/Integrify.Clients.Worker.Service/Scoped/DefaultScopedProcessingService.cs
+++ src/clients/worker/Integrify.Clients.Worker.Service/Scoped/DefaultScopedProcessingService.cs
@@ -1,4 +1,7 @@
 using Integrify.Integrations.Customers.Api.Public;
+using Integrify.Integrations.Orders.Api.Public;
+using Integrify.Integrations.Products.Api.Public;
+using Integrify.Integrations.Stocks.Api.Public;
 using Integrify.Shared.Abstractions.Integrations;
 using Integrify.Shared.Abstractions.Plugins;
 
@@ -7,15 +10,16 @@ namespace Integrify.Clients.Worker.Service.Scoped;
 public sealed class DefaultScopedProcessingService(
     IList<IIntegration> integrations,
     IList<IPlugin> plugins,
-    ILogger<DefaultScopedProcessingService> logger,
-    ICustomersIntegrationApi customersIntegrationApi)
-    : IScopedProcessingService
+    ICustomersIntegrationApi customersIntegrationApi,
+    IOrdersIntegrationApi ordersIntegrationApi,
+    IProductsIntegrationApi productsIntegrationApi,
+    IStocksIntegrationApi stocksIntegrationApi) : IScopedProcessingService
 {
     public async Task ProcessAsync(CancellationToken cancellationToken)
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            var command = ReadInput();
+            var command = ReadInput()?.ToLower();
 
             if (string.IsNullOrEmpty(command))
             {
@@ -23,7 +27,42 @@ public sealed class DefaultScopedProcessingService(
                 continue;
             }
 
-            switch (command.ToLower())
+            if (command.Contains("sync"))
+            {
+                var customersApi = nameof(customersIntegrationApi);
+                var ordersApi = nameof(ordersIntegrationApi);
+                var productsApi = nameof(productsIntegrationApi);
+                var stocksApi = nameof(stocksIntegrationApi);
+                
+                foreach (var integration in integrations)
+                {
+                    if (command.Contains(integration.Name.ToLower()))
+                    {
+                        if (customersApi.Contains(integration.Name.ToLower()))
+                        {
+                            await customersIntegrationApi.RunIntegration();
+                        }
+                        
+                        if (ordersApi.Contains(integration.Name.ToLower()))
+                        {
+                            await ordersIntegrationApi.RunIntegration();
+                        }
+                        
+                        if (productsApi.Contains(integration.Name.ToLower()))
+                        {
+                            await productsIntegrationApi.RunIntegration();
+                        }
+                        
+                        if (stocksApi.Contains(integration.Name.ToLower()))
+                        {
+                            await stocksIntegrationApi.RunIntegration();
+                        }
+                    }
+                }
+                
+            }
+            
+            switch (command)
             {
                 case "integrations":
                     PrintAvailableIntegrations();
