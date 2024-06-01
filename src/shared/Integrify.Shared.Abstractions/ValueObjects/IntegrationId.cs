dec374dd92caa37875cf531c294cced4f45138e2
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-06-01 18:52:10 +0200

    add value object for integration id

---
+++ src/shared/Integrify.Shared.Abstractions/ValueObjects/IntegrationId.cs
@@ -0,0 +1,7 @@
+﻿namespace Integrify.Shared.Abstractions.ValueObjects;
+
+/// <summary>
+/// Represents integration model entity unique identifier
+/// </summary>
+/// <param name="Id">Unique identifier</param>
+public record IntegrationId(Guid Id);
