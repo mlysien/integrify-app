99bd75958018d6e5175117db0b81da2de0fcf1ed
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-06-01 18:52:24 +0200

    add value object for integration timestamp

---
+++ src/shared/Integrify.Shared.Abstractions/ValueObjects/IntegrationTimestamp.cs
@@ -0,0 +1,7 @@
+﻿namespace Integrify.Shared.Abstractions.ValueObjects;
+
+/// <summary>
+/// Timestamp of last synchronized integration model entity
+/// </summary>
+/// <param name="Ticks"></param>
+public record IntegrationTimestamp(long Ticks);
