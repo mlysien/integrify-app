86c8a2b2c73f84999b9da8f9d6f5004f7731c350
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-06-02 20:31:38 +0200

    create IIntegrationRepository

---
+++ src/shared/Integrify.Shared.Abstractions/Integrations/IIntegrationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Integrify.Shared.Abstractions.ValueObjects;
+
+namespace Integrify.Shared.Abstractions.Integrations;
+
+/// <summary>
+/// Interface provide methods for get required data for integration area
+/// </summary>
+public interface IIntegrationRepository
+{
+    /// <summary>
+    /// Returns timestamp of last integration
+    /// </summary>
+    Task<IntegrationTimestamp> GetLastIntegrationTimestamp();
+    
+    /// <summary>
+    /// Updates timestamp of last integration
+    /// </summary>
+    /// <param name="timestamp">New timestamp</param>
+    Task UpdateLastIntegrationTimestamp(IntegrationTimestamp timestamp);
+}
