9f08eade878cedc6a00461914bc90a6f96cf487e
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2024-06-03 23:03:34 +0200

    add IInitializer to worker startup method

--- src/clients/cli/Integrify.Clients.Cli.Service/Program.cs
+++ src/clients/cli/Integrify.Clients.Cli.Service/Program.cs
@@ -1,18 +1,12 @@
 using Integrify.Clients.Cli.Interpreter;
 using Integrify.Clients.Cli.Service;
 using Integrify.Clients.Cli.Service.Services;
-using Integrify.Shared.Infrastructure.Integrations;
-using Integrify.Shared.Infrastructure.Logging;
-using Integrify.Shared.Infrastructure.Plugins;
-using Integrify.Shared.Infrastructure.Time;
+using Integrify.Shared.Infrastructure;
 
 const string integrationAssemblyPrefix = "Integrify.Integrations."; 
 const string pluginAssemblyPrefix = "Integrify.Plugins."; 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddClock();
-builder.Services.AddConsoleLogging();
-builder.Services.AddIntegrations(integrationAssemblyPrefix);
-builder.Services.AddPlugins(pluginAssemblyPrefix);
+builder.Services.AddInfrastructure(integrationAssemblyPrefix, pluginAssemblyPrefix);
 builder.Services.AddCommandsInterpreter();
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddScoped<IProcessingService, ProcessingService>();
--- src/clients/cli/Integrify.Clients.Cli.Service/Worker.cs
+++ src/clients/cli/Integrify.Clients.Cli.Service/Worker.cs
@@ -1,12 +1,20 @@
 using Integrify.Clients.Cli.Service.Services;
+using Integrify.Shared.Abstractions.Initializer;
 
 namespace Integrify.Clients.Cli.Service;
 
-public sealed class Worker(IServiceScopeFactory serviceScopeFactory, ILogger<Worker> logger) : BackgroundService
+public sealed class Worker(
+    ILogger<Worker> logger,
+    IInitializer initializer,
+    IServiceScopeFactory serviceScopeFactory) 
+    : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         logger.PrintBanner();
+        PrintInitializing();
+        await initializer.InitializeAsync();
+        
         logger.LogInformation("{0} service is running.", "Integrify");
 
         await RunAsync(cancellationToken);
@@ -25,5 +33,13 @@ public sealed class Worker(IServiceScopeFactory serviceScopeFactory, ILogger<Wor
         logger.LogInformation("{0} service has been stopped.", "Integrify");
         
         await base.StopAsync(stoppingToken);
+    }
+
+
+    private void PrintInitializing()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Initializing Integrify...");
+        Console.ResetColor();
     }
 }
